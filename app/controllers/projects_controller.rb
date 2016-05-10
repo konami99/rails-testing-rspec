6c8767ef171ebbb0b941e25ebc9e04357ddc4a8d
rchou <konami99@hotmail.com>
2016-05-10 23:35:53 +1000

    rspec

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,4 +3,17 @@ class ProjectsController < ApplicationController
     def new
         @project = Project.new
     end
+    
+    def create
+        @action = CreatesProject.new(
+            name: params[:project][:name],
+            task_string: params[:project][:tasks])
+            
+        @action.create   
+        redirect_to projects_path
+    end
+    
+    def index
+        @projects = Project.all
+    end
 end
